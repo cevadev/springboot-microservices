31599b42ec7fbfd6436360069b86115bb8382ae3
cevadev <carlos.villanueva.altuna@gmail.com>
2023-01-09 19:16:32 -0300

    Curso Spring Boot - Adding Testing

--- microservices/product-service/src/test/java/ar/admiral/microservices/core/product/ProductServiceApplicationTests.java
+++ microservices/product-service/src/test/java/ar/admiral/microservices/core/product/ProductServiceApplicationTests.java
@@ -1,13 +1,41 @@
 package ar.admiral.microservices.core.product;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
+
 	@Test
-	void contextLoads() {
+	void getProductById() {
+		int productId = 1;
+		client.get()
+				.uri("/product/" + productId)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody().jsonPath("$.productId").isEqualTo(productId);
 	}
 
+	@Test
+	void getProductInvalidParameterString() {
+		int productId = 1;
+		client.get()
+				.uri("/product/no-integer" + productId)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+					.jsonPath("$.path").isEqualTo("/product/no-integer");
+					//.jsonPath("$.message").isEqualTo("Type mismatch.");
+	}
 }
