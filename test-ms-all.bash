c87c5fe997c1c10aae7ea81248239aa4ca7421b5
cevadev <carlos.villanueva.altuna@gmail.com>
2023-01-28 17:55:50 -0300

    Curso Springboot - working with Docker & Docker Compose

--- test-ms-all.bash
+++ test-ms-all.bash
@@ -6,7 +6,9 @@
 
 # host y port con valores por defecto
 :${HOST=localhost}
-:${PORT=7000}
+# PUERTO PARA TRABAJAR CON DOCKER
+:${HOST=8080}
+#:${PORT=7000}
 
 :${PROD_ID_REVS_RECS=1}
 :${PROD_ID_NOT_FOUND=13}
@@ -55,20 +57,104 @@ funcion assertEqual(){
   fi
 }
 
+# probamos un url
+function testUrl() {
+    url = $@
+    if $url -ks -f -o /dev/null
+    then
+      return 0
+    else
+      return 1
+    fi;
+}
+
+function waitForService() {
+    # capturamos el archivo de la derecha
+    url = $@
+    echo -n "Wait for: $url..."
+    n=0
+    until testUrl $url
+    do
+      # n inicia en 0
+      n = $((n + 1))
+      if [[ $n == 100 ]]
+      then
+        echo "Give up"
+        exit 1
+      else
+        # ponemos un delay para que se levanten los 4 springboots
+        sleep 3
+        echo -n ", retry #$n"
+      fi
+    done
+
+    echo "DONE, continues..."
+}
+
 # establecemos el entorno
 set -e
+
+echo "Starting tests..." `date`
+
 # mostramos el valor elegido para host y port, o mostramos el valor por defecto
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
 
-# Verificamos un request normal, que espera las 3 recomendaciones y revisiones
-assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_PROD_ID_REVS_RECS -s"
+# validamos si le pasamos al archivo bash un argumento start. si es asi iniciamos los microservicios
+if [[ $@ == *"start"* ]]]
+then
+  # nos aseguramos en terminar cualquier contenedor de docker orphans
+  echo "Restarting the test environment..."
+  echo "docker-compose down --remove-orphans"
+  docker-compose down --remove-orphans
+  # levantamos todo
+  echo "docker-compose up -d"
+  docker-compose up -d
+fi
 
+## al momento de correr el comando docker-compose up -d habra unos segundos de espera hasta que logre inicar los microservicios
+## hacemos una espera hasta que el curl responda con la funcion waitForService
+wairForService curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS_RECS
+
+
+# Verificamos un request normal, que espera las 3 recomendaciones y revisiones
+assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS_RECS -s"
 # verificamos si el $PROD_ID_REVS_RECS es igual al productId que viene en el response
 assertEqual $PROD_ID_REVS_RECS $(echo $RESPONSE | jq .productId)
-
 # si la llamade es OK entonces debemos tener 3 reviews y 3 recommendations
 assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
 assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
+
+# verificamos que un 404 (not found) error se retorne para un productId no existente
+assertCurl 404 "curl https://$HOST:$PORT/product-composite/$PROD_ID_NOT_FOUND -s"
+assertEqual "No product found for productId: $PROD_ID_NOT_FOUND" "$(echo $RESPONSE | jq -r .message)"
+
+# verificamos que no se retornan recomendaciones para el productId $PROD_ID_NO_RECS
+assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NO_RECS -s"
+assertEqual $PROD_ID_NO_RECS $(echo $RESPONSE | jq .productId)
+assertEqual 0 $(echo $RESPONSE | jq ".recommendations | length")
+assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
+
+# verificamos que no se retornan reviews para el productId $PROD_ID_NO_REVS
+assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NO_REVS -s"
+assertEqual $PROD_ID_NO_REVS $(echo $RESPONSE | jq .productId)
+assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
+assertEqual 0 $(echo $RESPONSE | jq ".reviews | length")
+
+# verificamos que un 422(unprocessable entity) retorna un error para el productId
+assertCurl 422 "curl http://$HOST:$PORT/product-composite/-1 s"
+assertEqual "\"Invalid productId: -1\"" "$(echo $RESPONSE | jq .message)"
+
+# verificamos que un 400 (bad request) retorna un error para un productId que no esta en el rangi
+assertCurl 400 "curl http://$HOST:$PORT/product-composite/no-integer s"
+assertEqual "\"Type mismatch.\"" "$(echo $RESPONSE | jq .message)"
+
+# si se le pasa al archivo bash el argumento stop, detenemos el docker
+if [[ $@ == *"stop"* ]]
+then
+  echo "We are done, stopping the test environment..."
+  echo "$ docker-compose down"
+  docker-compose down
+fi
 
 echo "End, all test OK: " `date`
