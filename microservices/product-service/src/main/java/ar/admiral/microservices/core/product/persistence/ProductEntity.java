cd230cf723ee1ae0c8fcf788cd905a08a1e97ab7
cevadev <carlos.villanueva.altuna@gmail.com>
2023-04-10 20:19:24 -0300

    Curso Spring Boot

---
+++ microservices/product-service/src/main/java/ar/admiral/microservices/core/product/persistence/ProductEntity.java
@@ -0,0 +1,65 @@
+package ar.admiral.microservices.core.product.persistence;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+public class ProductEntity {
+    @Id private String id;
+    @Version private Integer version;
+
+    @Indexed(unique = true)
+    private int productID;
+    private String name;
+    private int weight;
+
+    public ProductEntity(){}
+
+    public ProductEntity(int productID, String name, int weight){
+        this.productID = productID;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
